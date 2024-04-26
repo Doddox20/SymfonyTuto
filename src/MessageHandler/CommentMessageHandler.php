a9e1a4b1bcb9942e19f8c01ef868364c160da0f3
Dorian Contal <dorian.contal@epitech.eu>
2024-04-26 11:40:01 +0200

    Essaie test commenatire auto

--- src/MessageHandler/CommentMessageHandler.php
+++ src/MessageHandler/CommentMessageHandler.php
@@ -3,17 +3,27 @@ namespace App\MessageHandler;
 use App\Message\CommentMessage;
 use App\Repository\CommentRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
-
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Workflow\WorkflowInterface;
 class CommentMessageHandler implements MessageHandlerInterface
 {
     private $commentRepository;
     private $entityManager;
+    private $bus;
+    private $workflow;
+    private $logger;
 
-    public function __construct(CommentRepository $commentRepository, EntityManagerInterface $entityManager)
+    public function __construct(CommentRepository $commentRepository, EntityManagerInterface $entityManager, MessageBusInterface $bus, WorkflowInterface $commentStateMachine, LoggerInterface $logger)
     {
         $this->commentRepository = $commentRepository;
         $this->entityManager = $entityManager;
+        $this->bus = $bus;
+        $this->workflow = $commentStateMachine;
+        $this->logger = $logger;
+    
+   
     }
 
     public function __invoke(CommentMessage $message)
@@ -22,8 +32,18 @@ class CommentMessageHandler implements MessageHandlerInterface
         if (!$comment) {
             return;
         }
+        if ($this->workflow->can($comment, 'accept')) {
+                        $transition = 'accept';
+                        $this->workflow->apply($comment, $transition);
+                        $this->entityManager->flush();
+            
+                        $this->bus->dispatch($message);
+                    } elseif ($this->workflow->can($comment, 'publish') || $this->workflow->can($comment, 'publish_ham')) {
+                                    $this->workflow->apply($comment, $this->workflow->can($comment, 'publish') ? 'publish' : 'publish_ham');
+                                    $this->entityManager->flush();
 
-        $comment->setState('published');
-        $this->entityManager->flush();
+                    } elseif ($this->logger) {
+                        $this->logger->debug('Dropping comment message', ['comment' => $comment->getId(), 'state' => $comment->getState()]);
+                    }
     }
 }
